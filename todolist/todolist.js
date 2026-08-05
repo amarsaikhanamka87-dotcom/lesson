89d8d3ccf9728110fc1ea9b38bc5ce34f3be9201
26LP89141092 <26LP89141092@G304CC0215.local>
2026-08-05 17:44:44 +0800

    todolist update

--- todolist/todolist.js
+++ todolist/todolist.js
@@ -2,6 +2,34 @@ const input = document.querySelector(".header input");
 const addBtn = document.querySelector(".header button");
 const list = document.querySelector(".list");
 
+const allBtn = document.querySelector(".All");
+const activeBtn = document.querySelector(".Active");
+const completedBtn = document.querySelector(".Completed");
+
+allBtn.addEventListener("click", () => {
+  if (activeBtn && completedBtn) {
+    activeBtn.classList.remove("allBackGround");
+    completedBtn.classList.remove("allBackGround");
+  }
+  allBtn.classList.add("allBackGround");
+});
+
+activeBtn.addEventListener("click", () => {
+  if (allBtn && completedBtn) {
+    allBtn.classList.remove("allBackGround");
+    completedBtn.classList.remove("allBackGround");
+  }
+  activeBtn.classList.add("allBackGround");
+});
+
+completedBtn.addEventListener("click", () => {
+  if (activeBtn && allBtn) {
+    activeBtn.classList.remove("allBackGround");
+    allBtn.classList.remove("allBackGround");
+  }
+  completedBtn.classList.add("allBackGround");
+});
+
 addBtn.addEventListener("click", () => {
   const textValue = input.value;
 
@@ -20,6 +48,19 @@ addBtn.addEventListener("click", () => {
   checkBox.addEventListener("click", () => {
     if (checkBox.checked) {
       textContainer.classList.add("zuraas");
+    } else {
+      textContainer.classList.remove("zuraas");
+    }
+  });
+
+  completedBtn.addEventListener("click", () => {
+    if (activeBtn && allBtn) {
+      activeBtn.classList.remove("allBackGround");
+      allBtn.classList.remove("allBackGround");
+    }
+    completedBtn.classList.add("allBackGround");
+    if (checkBox.checked) {
+      completedBtn.document.createElement("div");
     }
   });
 
