f9cf550c8190d5b9249f92a37b4642a81400fdb5
26LP89141092 <26LP89141092@G304CC0215.local>
2026-08-06 13:18:24 +0800

    Bagshaa

--- todolist/todolist.js
+++ todolist/todolist.js
@@ -6,12 +6,34 @@ const allBtn = document.querySelector(".All");
 const activeBtn = document.querySelector(".Active");
 const completedBtn = document.querySelector(".Completed");
 
+const count = document.querySelector(".footerCount");
+
+const updateCount = () => {
+  const allTodos = document.querySelectorAll(".item");
+  const totalTasks = allTodos.length;
+
+  let completedCount = 0;
+
+  for (let i = 0; i < allTodos.length; i++) {
+    const checkBox = allTodos[i].querySelector("input");
+    if (checkBox && checkBox.checked) {
+      completedCount++;
+    }
+    count.textContent = `${completedCount} of ${totalTasks} tasks completed`;
+  }
+};
 allBtn.addEventListener("click", () => {
   if (activeBtn && completedBtn) {
     activeBtn.classList.remove("allBackGround");
     completedBtn.classList.remove("allBackGround");
   }
   allBtn.classList.add("allBackGround");
+
+  const alltodos = document.querySelectorAll(".item");
+
+  for (let i = 0; i < alltodos.length; i++) {
+    alltodos[i].style.display = "flex";
+  }
 });
 
 activeBtn.addEventListener("click", () => {
@@ -20,6 +42,18 @@ activeBtn.addEventListener("click", () => {
     completedBtn.classList.remove("allBackGround");
   }
   activeBtn.classList.add("allBackGround");
+
+  const activetodos = document.querySelectorAll(".item");
+
+  for (let i = 0; i < activetodos.length; i++) {
+    const checkBox = activetodos[i].querySelector("input");
+
+    if (checkBox.checked === true) {
+      activetodos[i].style.display = "none";
+    } else {
+      activetodos[i].style.display = "flex";
+    }
+  }
 });
 
 completedBtn.addEventListener("click", () => {
@@ -28,6 +62,18 @@ completedBtn.addEventListener("click", () => {
     allBtn.classList.remove("allBackGround");
   }
   completedBtn.classList.add("allBackGround");
+
+  const todos = document.querySelectorAll(".item");
+
+  for (let i = 0; i < todos.length; i++) {
+    const checkBox = todos[i].querySelector("input");
+
+    if (checkBox.checked == true) {
+      todos[i].style.display = "flex";
+    } else {
+      todos[i].style.display = "none";
+    }
+  }
 });
 
 addBtn.addEventListener("click", () => {
@@ -51,26 +97,18 @@ addBtn.addEventListener("click", () => {
     } else {
       textContainer.classList.remove("zuraas");
     }
+    updateCount();
   });
 
-  completedBtn.addEventListener("click", () => {
-    if (activeBtn && allBtn) {
-      activeBtn.classList.remove("allBackGround");
-      allBtn.classList.remove("allBackGround");
-    }
-    completedBtn.classList.add("allBackGround");
-    if (checkBox.checked) {
-      completedBtn.document.createElement("div");
-    }
-  });
-
-  deleteBtn.addEventListener("click", () => {
-    itemContainer.remove();
-  });
   itemContainer.appendChild(checkBox);
   itemContainer.appendChild(textContainer);
   itemContainer.appendChild(deleteBtn);
   list.appendChild(itemContainer);
 
+  deleteBtn.addEventListener("click", () => {
+    itemContainer.remove();
+  });
+
   input.value = "";
+  updateCount();
 });
